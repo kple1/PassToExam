2806efd427de1c10e50f6b4a8998aa14df672ce3
kple1 <kple1@naver.com>
2023-06-27 19:45:12 +0900

    1학기 파이썬 마감

--- 4-n/4-18.py
+++ 4-n/4-18.py
@@ -1,13 +1,21 @@
-print("숫자 맞추기 게임입니다 1~100")
+import random
+
+print("숫자 맞추기 게임 입니다 1~100")
+
 var0 = 0
+var2 = random.randint(1, 100000000)
+List = [var2]
 
 while True:
     var0 += 1
-    var1 = int(input("{}번째 시도 : ".format(var0)))
-    if var1 > 14:
+    var1 = input("{}번째 시도 : ".format(var0))
+    if var1 == "미리보기":
+        print(List)
+
+    elif int(var1) > var2:
         print("입력하신 숫자보다 작습니다.")
-    elif var1 < 14:
+    elif int(var1) < var2:
         print("입력하신 숫자보다 큽니다.")
-    else:
+    elif int(var1) == var2:
         print("정답입니다.")
         break
